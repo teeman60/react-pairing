fe0dab6b7879ace5d943407846b2e5db59344767
joshua-miles <joshua@christetulit.org>
2020-05-12 16:45:40 -0500

    -

--- src/PoemList.js
+++ src/PoemList.js
@@ -3,7 +3,7 @@ import { PoemCard } from './PoemCard';
 
 export function PoemList(){
     return (
-        <div className="ui cards">
+        <div className="ui cards" style={{ marginTop: 20 }}>
         {[ /*  Replace this with an array of poems */ ].map( poem => (
             <PoemCard
                 name={poem.name}
